4af6c73f8cf51e3b6539c6d3d5f18cb91643f8c1
ShaniCattan <323shani@gmail.com>
2020-06-14 17:32:35 +0300

    added async to functions

--- wwwroot/js/script.js
+++ wwwroot/js/script.js
@@ -8,7 +8,6 @@ let mapAllPlanes = new Map();
 let currFlightId = "";
 let polyline;
 
-
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
